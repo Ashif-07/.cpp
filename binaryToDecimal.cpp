38cbc3c54b42dd3d6df0bab8b8f96a12364efe1b
MD ASHIF ANSARI <mdashifansari454@gmail.com>
2025-08-06 15:13:18 +0530

    binaryToDecimal.cpp

---
+++ binaryToDecimal.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+/**/ #include<cmath>
+using namespace std;
+int binarytodecimal(int n)
+{
+    int ans = 0, /*mul = 1;*/ pw = 0;
+    while (n > 0)
+    {
+        int digit = n % 10;
+        ans += digit * /*mul*/ pow(2, pw);
+        n /= 10;
+        pw++;
+        // mul *= 2;
+    }
+    return ans;
+}
+int main()
+{
+    int n;
+    cout << "Enter a Binary Number : ";
+    cin >> n;
+    cout << "Decimal : " << binarytodecimal(n);
+    return 0;
+}
